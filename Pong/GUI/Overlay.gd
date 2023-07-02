0c916b9ec891d4db97d866e56907202dc6a4f0a0
eunoiad <126207927+eunoiad@users.noreply.github.com>
2023-07-02 18:52:38 -0400

    Update Overlay.gd

--- Pong/GUI/Overlay.gd
+++ Pong/GUI/Overlay.gd
@@ -4,7 +4,7 @@ func _ready():
 	$"Player 1".text = "Score: " + str(Singleton.paddle1_score)
 	$"Player 2".text = "Score: " + str(Singleton.paddle2_score)
 	$Countdown.hide()
-	await get_tree().create_timer(Singleton.score_fade_time, false, true).timeout
+	await get_tree().create_timer(Singleton.score_fade_time + 3.0, false, true).timeout
 	var tween = get_tree().create_tween()
 	tween.parallel().tween_property($"Player 1", "modulate", Color.hex(00000000), 1).set_trans(Tween.TRANS_SINE)
 	tween.parallel().tween_property($"Player 2", "modulate", Color.hex(00000000), 1).set_trans(Tween.TRANS_SINE)
